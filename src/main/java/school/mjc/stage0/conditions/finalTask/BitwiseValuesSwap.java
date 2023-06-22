40de8051d770840e39cd7856b6a8d15b4df41328
zurazumba <zura.zumbadze@yahoo.com>
2023-06-22 22:34:32 +0400

    finalconditions

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -3,17 +3,19 @@ package school.mjc.stage0.conditions.finalTask;
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
         first = first ^ second;
+
         second = first ^ second;
         first = first ^ second;
-//        System.out.print(first);
-        System.out.print(first+"\n");
-        System.out.print(second+"\n");
+        System.out.println(first);
+        System.out.println(second);
+
+
 
     }
     public static void main (String [] args){
         BitwiseValuesSwap bitwiseValuesSwap = new BitwiseValuesSwap();
-        int first = 3;
-        int second = 5;
+        int first = 100;
+        int second = -100;
         bitwiseValuesSwap.swap(first, second);
     }
 }
